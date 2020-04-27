70e81484b7270b97ac676e7201fb8a4546252591
JSKuba <jakub1chmielewski@gmail.com>
2020-04-27 19:36:49 +0200

    Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,41 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/Layout'
 import '../styles/index.scss'
 import indexStyles from '../styles/index-main.module.scss'
 import Main from '../components/Main'
 
-const IndexPage = () => {
+const IndexPage = (props) => {
+    const img = useStaticQuery(graphql`
+        query {
+            allFile (
+                filter: { ext: { ne: ".md" } }
+            ) {
+                edges {
+                    node {
+                        name
+                        absolutePath
+                    }
+                }
+            }
+        }
+    `)
+
+    let absolutePath = ''
+
+    img.allFile.edges.map(v => {
+        if (v.node.name === props.pageContext.slug) {
+            absolutePath = v.node.absolutePath
+        }
+        return v
+    })
+
     return (
         <>
             <Helmet>
-                <meta property="og:image" content="./img/fat_cat.jpeg" />
+                <meta property="og:image" content={!absolutePath === '' && absolutePath}/>
             </Helmet>
             <div className={indexStyles.wrapper}>
                 <Layout>
