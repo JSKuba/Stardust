a59b4e9cba452f1d5f5bf23911b9828d68db271c
JSKuba <jakub1chmielewski@gmail.com>
2020-04-27 21:44:58 +0200

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,19 @@ import Main from '../components/Main'
 const IndexPage = (props) => {
 
     let helmet
-    if(props.pageContext.slug === '') {
+    if(props.pageContext.slug !== undefined) {
+        console.log('hi')
         helmet = <Helmet>
-                            <title>Stardust</title>
-                            <html lang="en" xmlns="http://www.w3.org/1999/xhtml" prefix="og: http://ogp.me/ns# fb: http://www.facebook.com/2008/fbml" />
-                            <meta property="og:image" content={`https://github.com/JSKuba/gatsby-netlify/tree/master/src/md-data/main/${props.pageContext.slug}/${props.pageContext.slug}.jpg`}/>
-                            <meta property="og:title" content={`Stardust - ${props.pageContext.slug}`} />
-                            <meta property="og:site_name" content="Stardust" />
-                            <meta property="og:url" content={`https://jskuba.netlify.app/${props.pageContext.slug}`}/>
-                            <meta property="og:type" content="website" />
-                        </Helmet>
+                    <title>Stardust</title>
+                    <html lang="en" xmlns="http://www.w3.org/1999/xhtml" prefix="og: http://ogp.me/ns# fb: http://www.facebook.com/2008/fbml" />
+                    <meta property="og:image" content={`https://github.com/JSKuba/gatsby-netlify/tree/master/src/md-data/main/${props.pageContext.slug}/${props.pageContext.slug}.jpg`}/>
+                    <meta property="og:title" content={`Stardust - ${props.pageContext.slug}`} />
+                    <meta property="og:site_name" content="Stardust" />
+                    <meta property="og:url" content={`https://jskuba.netlify.app/${props.pageContext.slug}`}/>
+                    <meta property="og:type" content="website" />
+                </Helmet>
     } else {
+        console.log('ih')
         helmet = <Helmet>
                     <title>Stardust</title>
                     <html lang="en" xmlns="http://www.w3.org/1999/xhtml" prefix="og: http://ogp.me/ns# fb: http://www.facebook.com/2008/fbml" />
