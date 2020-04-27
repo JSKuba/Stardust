acab20518f9ad500f36849864e1e451fe85d5f0c
JSKuba <jakub1chmielewski@gmail.com>
2020-04-27 21:34:44 +0200

    Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import { graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/Layout'
 import '../styles/index.scss'
@@ -8,41 +7,33 @@ import indexStyles from '../styles/index-main.module.scss'
 import Main from '../components/Main'
 
 const IndexPage = (props) => {
-    const img = useStaticQuery(graphql`
-        query {
-            allFile (
-                filter: { ext: { ne: ".md" } }
-            ) {
-                edges {
-                    node {
-                        name
-                        absolutePath
-                    }
-                }
-            }
-        }
-    `)
 
-    let absolutePath = ''
-
-    img.allFile.edges.map(v => {
-        if (v.node.name === props.pageContext.slug) {
-            absolutePath = v.node.absolutePath
-        }
-        return v
-    })
+    let helmet
+    if(props.pageContext.slug === '') {
+        helmet = <Helmet>
+                            <title>Stardust</title>
+                            <html lang="en" xmlns="http://www.w3.org/1999/xhtml" prefix="og: http://ogp.me/ns# fb: http://www.facebook.com/2008/fbml" />
+                            <meta property="og:image" content={`https://github.com/JSKuba/gatsby-netlify/tree/master/src/md-data/main/${props.pageContext.slug}/${props.pageContext.slug}.jpg`}/>
+                            <meta property="og:title" content={`Stardust - ${props.pageContext.slug}`} />
+                            <meta property="og:site_name" content="Stardust" />
+                            <meta property="og:url" content={`https://jskuba.netlify.app/${props.pageContext.slug}`}/>
+                            <meta property="og:type" content="website" />
+                        </Helmet>
+    } else {
+        helmet = <Helmet>
+                    <title>Stardust</title>
+                    <html lang="en" xmlns="http://www.w3.org/1999/xhtml" prefix="og: http://ogp.me/ns# fb: http://www.facebook.com/2008/fbml" />
+                    <meta property="og:image" content="https://github.com/JSKuba/gatsby-netlify/tree/master/src/components/img/logo.png" />
+                    <meta property="og:title" content="Stardust" />
+                    <meta property="og:site_name" content="Stardust" />
+                    <meta property="og:url" content="https://jskuba.netlify.app"/>
+                    <meta property="og:type" content="website" />
+                </Helmet>
+    }
 
     return (
         <>
-            <Helmet>
-                <title>Stardust</title>
-                <html lang="en" xmlns="http://www.w3.org/1999/xhtml" prefix="og: http://ogp.me/ns# fb: http://www.facebook.com/2008/fbml" />
-                <meta property="og:image" content={absolutePath === '' ? null : absolutePath}/>
-                <meta property="og:title" content={`${props.pageContext.slug}`} />
-                <meta property="og:site_name" content="Stardust" />
-                <meta property="og:url" content={`https://jskuba.netlify.app/${props.pageContext.slug}`}/>
-                <meta property="og:type" content="website" />
-            </Helmet>
+            {helmet}
             <div className={indexStyles.wrapper}>
                 <Layout>
                     <Main />
